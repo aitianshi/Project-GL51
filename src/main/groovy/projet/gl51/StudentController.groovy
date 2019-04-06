0d25cfb45bc333e973735b2930277708268fa632
Etienne Filhol <etienne.filhol@hotmail.fr>
2019-04-06 21:26:16 +0200

    initial commit

---
+++ src/main/groovy/projet/gl51/StudentController.groovy
@@ -0,0 +1,15 @@
+package projet.gl51
+
+import io.micronaut.http.annotation.Controller
+import io.micronaut.http.annotation.Get
+import io.micronaut.http.HttpStatus
+
+
+@Controller("/student")
+class StudentController {
+
+    @Get("/")
+    HttpStatus index() {
+        return HttpStatus.OK
+    }
+}
